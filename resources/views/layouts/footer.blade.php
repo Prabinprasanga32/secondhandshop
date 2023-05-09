fed3492c3392c7d29fedd0792150d228fca5942e
tyagisen <tyagisen@gmail.com>
2023-05-09 15:28:34 +0100

    SS-6 add frontend for nav bar, footer and product view page

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="./css/footerstyle.css">
+</head>
+<body>
+<footer>
+  <nav>
+    <ul>
+      <li><a style="font-weight:bold" href="#">SecondhandShop</a></li>
+      <li><a href="post">Buy Product</a></li>
+      <li><a href="post">Post Product</a></li>
+    </ul>
+  </nav>
+  <p>&copy; 2023 SecondhandShop. All rights reserved.</p>
+</footer>
+</body>
+</html>
