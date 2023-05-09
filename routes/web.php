03e581da10eee4028261bd31c4acfad023a061ae
SunamBhattarai <sunambhattarai1234@gmail.com>
2023-05-09 17:24:43 +0100

    SS-5 Add to create frontend and backend for product post page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,7 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/post', function () {
+    return view('layouts/productpost');
+});
 
 Auth::routes();
+Route::POST('add_product',[ProductController::class,'store'])->name('add_product');
+Route::get('show_product',[ProductController::class,'show'])->name('show_product');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
